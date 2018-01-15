0575359c07712582a740b1d986c34271ebb59fed
Esteban Herrera <esteban@driveshop.com>
2018-01-15 00:05:19 -0800

    I can read every file, and create a frequency dictionary. Now need to polish the variations (capitalization etc...), blacklist some shorter words, and do the actual calculations

--- tfidf.py
+++ tfidf.py
@@ -1,8 +1,33 @@
+#!/usr/bin/python
+
 import os
 
+# this function take an array of strings and 
+# returns a dictionary containing each word and a count of times that word is seen
+def make_histogram(list):
+    histogram = {}
+    print "got here"
+    print list
+    for word in list:
+        # TODO: consider trailing punctuation and capitalization
+        print word
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+    # print histogram
+    return histogram
+
+
+# open each file
 for filename in os.listdir("./sona"):
-    print(filename)
+    # print(filename)
     path = "./sona/" + filename
     file = open(path, "r")
-    print file.read()
+    # file is open, now split and read into an array
+    data = file.read().split()
+    # count instances of each word
+    h = make_histogram(data)
+    print h
     print "-----"
+    #do it again
