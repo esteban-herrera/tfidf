0e653df6752a4ed921bfb0f96a876e8827afb531
Bulmaro Herrera <bulmaro@Herrera-MacAir.station>
2018-01-20 20:21:54 +0100

    some basic tests, and moved directories into a 'samples'

--- testcases.py
+++ testcases.py
@@ -1,8 +1,16 @@
 import unittest
+import tfidf
 
 class TestTFIDF(unittest.TestCase):
-    def test_true(self):
-        self.assertEqual("TRUE", "TRUE")
+    def test_nofiles(self):
+        tfidf.count_dir("nofiles")
+        self.assertTrue(len(tfidf.counts)==0, "no files in dir")
+    def test_empty(self):
+        tfidf.count_dir("empty")
+        self.assertTrue(len(tfidf.counts)==1, "single empty file")
+    def test_word1(self):
+        tfidf.count_dir("word_1")
+        self.assertTrue(len(tfidf.counts)==1, "single file with 1 word")
 
-if __name__ == '__maiin__':
+if __name__ == '__main__':
     unittest.main()
